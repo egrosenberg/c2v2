ce55fcb79af824219685549f417993e45b8c2319
egrosenberg <erosenberg2019@hotmail.com>
2026-02-02 21:52:37 -0500

    feat(api) add update-skill

--- src/app/api/skills/index.ts
+++ src/app/api/skills/index.ts
@@ -1,6 +1,7 @@
 import { getSkill } from "@db/services/skills/get-skill";
 import type { ServiceMetaFn } from "..";
 import { findSkills } from "@db/services/skills/find-skills";
+import type { updateSkill } from "@db/services/skills/update-skill";
 
 const route = "/api/skills";
 
@@ -14,4 +15,10 @@ export const svcFindSkills: ServiceMetaFn<typeof findSkills> = () => ({
   method: "GET",
   route,
   name: "find-skills",
+});
+
+export const svcUpdateSkill: ServiceMetaFn<typeof updateSkill> = () => ({
+  method: "POST",
+  route,
+  name: "update-skill",
 });
--- src/app/api/skills/route.ts
+++ src/app/api/skills/route.ts
@@ -2,6 +2,7 @@ import { NextRequest } from "next/server";
 import { getRouteParams } from "../options-transforms";
 import { getSkill } from "@db/services/skills/get-skill";
 import { findSkills } from "@db/services/skills/find-skills";
+import { updateSkill } from "@db/services/skills/update-skill";
 
 export async function GET(request: NextRequest) {
   try {
@@ -14,6 +15,24 @@ export async function GET(request: NextRequest) {
       }
       case "find-skills": {
         const data = await findSkills(options);
+        return Response.json({ data });
+      }
+      default:
+        throw new Error(`Unknown route: '${name}'`);
+    }
+  } catch (e) {
+    const error = e as Error;
+    return Response.json({ error: error.message }, { status: 400 });
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const { name, options } = await getRouteParams(request);
+
+    switch (name) {
+      case "update-skill": {
+        const data = await updateSkill(options);
         return Response.json({ data });
       }
       default:
